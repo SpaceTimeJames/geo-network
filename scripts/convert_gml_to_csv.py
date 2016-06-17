dfdac5764236ce9301e7997443b6de4a7a4b4473
Gabriel <gabriel.rosser@gmail.com>
2016-06-17 18:59:48 +0100

    Add outfile option to conversion script

--- scripts/convert_gml_to_csv.py
+++ scripts/convert_gml_to_csv.py
@@ -7,4 +7,5 @@ from converter import gml_to_node_edge_list
 
 if __name__ == '__main__':
     in_file = sys.argv[1]
-    res = gml_to_node_edge_list(in_file, routing=True)
+    outfile = sys.argv[2] if len(sys.argv) > 2 else None
+    res = gml_to_node_edge_list(in_file, outfile=outfile, routing=True)
