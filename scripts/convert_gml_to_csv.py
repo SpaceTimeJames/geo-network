848084370b28076150c9e06b05f351404861d9aa
Gabriel <gabriel.rosser@gmail.com>
2016-06-17 17:12:41 +0100

    Add path to script

--- scripts/convert_gml_to_csv.py
+++ scripts/convert_gml_to_csv.py
@@ -1,5 +1,8 @@
-from converter import gml_to_node_edge_list
 import sys
+import os
+sys.path.append(os.path.abspath(os.path.curdir))
+
+from converter import gml_to_node_edge_list
 
 
 if __name__ == '__main__':
